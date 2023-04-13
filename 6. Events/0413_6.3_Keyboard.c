28e16a46fae1cc06c7e225623d9b8e7ba997432c
nae-room <12191726@inha.edu>
2023-04-13 22:58:51 +0900

    6.3 6.4 성공 ~

---
+++ 6. Events/0413_6.3_Keyboard.c
@@ -0,0 +1,206 @@
+// 6.3 Keyboard
+// Quit, Zoom, Reset to initial view
+// Double buffering 완료 
+
+#include <stdlib.h>
+#include <malloc.h>
+#include "GUI.h"
+
+#define pi 3.1415956
+
+int zoom = 0;
+float zm;
+
+typedef struct mesh
+{
+    int nv;
+    float *vertics;
+    int nt;
+    int *triangle;
+} Mesh;
+
+Mesh msh;
+
+void init(void)
+{
+    glClearColor(0.0 , 0.0, 0.0, 0.0);
+    glColor3f(1.0, 1.0 ,1.0);
+}
+
+void Geometry(void)
+{
+    msh.nv = 8;
+    msh.vertics = (float *) malloc(msh.nv*3*sizeof(float));
+    msh.vertics[0] = 0.0;
+    msh.vertics[1] = 0.0;
+    msh.vertics[2] = 0.0;
+
+    msh.vertics[3] = 1.0;
+    msh.vertics[4] = 0.0;
+    msh.vertics[5] = 0.0;
+
+    msh.vertics[6] = 1.0;
+    msh.vertics[7] = 1.0;
+    msh.vertics[8] = 0.0;
+
+    msh.vertics[9] = 0.0;
+    msh.vertics[10] = 1.0;
+    msh.vertics[11] = 0.0;
+
+    msh.vertics[12] = 0.0;
+    msh.vertics[13] = 0.0;
+    msh.vertics[14] = 1.0;
+
+    msh.vertics[15] = 1.0;
+    msh.vertics[16] = 0.0;
+    msh.vertics[17] = 1.0;
+
+    msh.vertics[18] = 1.0;
+    msh.vertics[19] = 1.0;
+    msh.vertics[20] = 1.0;
+
+    msh.vertics[21] = 0.0;
+    msh.vertics[22] = 1.0;
+    msh.vertics[23] = 1.0;
+
+    msh.nt = 12;
+    msh.triangle = (int *) malloc(msh.nt*3*sizeof(int));
+    msh.triangle[0] = 2;
+    msh.triangle[1] = 3;
+    msh.triangle[2] = 6;
+
+    msh.triangle[3] = 3;
+    msh.triangle[4] = 7;
+    msh.triangle[5] = 6;
+
+    msh.triangle[6] = 3;
+    msh.triangle[7] = 4;
+    msh.triangle[8] = 7;
+
+    msh.triangle[9] = 4;
+    msh.triangle[10] = 8;
+    msh.triangle[11] = 7;
+
+    msh.triangle[12] = 4;
+    msh.triangle[13] = 1;
+    msh.triangle[14] = 8;
+
+    msh.triangle[15] = 1;
+    msh.triangle[16] = 5;
+    msh.triangle[17] = 8;
+
+    msh.triangle[18] = 1;
+    msh.triangle[19] = 2;
+    msh.triangle[20] = 5;
+
+    msh.triangle[21] = 2;
+    msh.triangle[22] = 6;
+    msh.triangle[23] = 5;
+
+    msh.triangle[24] = 5;
+    msh.triangle[25] = 6;
+    msh.triangle[26] = 7;
+
+    msh.triangle[27] = 7;
+    msh.triangle[28] = 8;
+    msh.triangle[29] = 5;
+
+    msh.triangle[30] = 3;
+    msh.triangle[31] = 2;
+    msh.triangle[32] = 1;
+
+    msh.triangle[33] = 1;
+    msh.triangle[34] = 4;
+    msh.triangle[35] = 3;
+}
+
+void ViewMatrix(void)
+{
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(4, 3, 3, 0.5, 0.5, 0.5, 0, 0, 1);
+}
+
+void ProjectionMatrix(void)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(zoom == 0) glFrustum(-0.5, 0.5, -0.5, 0.5, 1.0, 100.0);
+    if(zoom == 1) glFrustum(-0.5+zm, 0.5-zm, -0.5+zm, 0.5-zm, 1.0, 100.0);
+}
+
+void DrawObject(void)
+{
+    int i, j, s;
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glBegin(GL_TRIANGLES);
+    for(i=1; i<=msh.nt; i++)
+    {
+        for(j=0; j<=2; j++)
+        {
+            glColor3f(1.0, 1.0, 1.0);
+            s = msh.triangle[3*(i-1)+j];
+            glVertex3f(msh.vertics[3*(s-1)], msh.vertics[3*(s-1)+1],msh.vertics[3*(s-1)+2]);
+        }
+    }
+    glEnd();
+}
+
+void PutScreen(void)
+{
+    glutSwapBuffers();
+}
+
+void Display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    ViewMatrix();
+    ProjectionMatrix();
+
+    DrawObject();
+    PutScreen();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if(key == 'q' || key == 'Q') exit(0);
+    if(key == 'z')
+    {
+        zoom = 1;
+        zm -= 0.03;
+        if (zm >= 0.5) zm = 0.5;
+    }
+    if(key == 'Z')
+    {
+        zoom = 1;
+        zm += 0.03;
+        if (zm >= 0.5) zm = 0.5;
+    }
+    if(key == 'i' || key == 'I')
+    {
+        zoom = 0;
+        zm = 0;
+    }
+    glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    Geometry();
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(10, 10);
+    glutCreateWindow("LO13_Kim Da-Yeon");
+
+    init();
+
+    glutDisplayFunc(Display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+    return 0;
+}
